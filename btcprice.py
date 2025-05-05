cdd02064f194c6aa67bfde7852fed19f90944141
poplop03 <thienductang@gmail.com>
2025-05-05 20:52:56 +0700

    divided

--- btcprice.py
+++ btcprice.py
@@ -1,12 +1,17 @@
 import requests
 import time
-symbol = 'BTCUSDT'
-api_url = 'https://api.api-ninjas.com/v1/cryptoprice?symbol={}'.format(symbol)
-response = requests.get(api_url, headers={'X-Api-Key': 'mdLctKthWCR2lN6ElcChEg==WjovL7Hr7W9TGaMg'})
 
-while 1: 
+symbol = 'BTCUSDT'
+api_url = f'https://api.api-ninjas.com/v1/cryptoprice?symbol={symbol}'
+headers = {'X-Api-Key': 'mdLctKthWCR2lN6ElcChEg==WjovL7Hr7W9TGaMg'}
+
+while True: 
+    response = requests.get(api_url, headers=headers)
+    
     if response.status_code == requests.codes.ok:
-        print(response.text)
+        data = response.json()  # Convert response.text to a Python dict
+        print(f"Symbol: {data['symbol']} | Price: {data['price']}")
     else:
         print("Error:", response.status_code, response.text)
+    
     time.sleep(1)
